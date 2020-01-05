37f5932dec5645c1ba59dfb5b61f3ab52be58d7f
doubix <adib.grouz@gmail.com>
2020-01-05 17:26:36 +0100

    [fix] stderr  error message

--- stack_p.c
+++ stack_p.c
@@ -49,13 +49,13 @@ void pchar(stack_t **stack, unsigned int line_number)
 
 if (stack == NULL || *stack == NULL)
 {
-printf("L%d: can't pchar, stack empty\n", line_number);
+fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 exit(EXIT_FAILURE);
 }
 
 if ((*stack)->n < 0 || (*stack)->n > 127)
 {
-printf("L%d: can't pchar, value out of range\n", line_number);
+fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 exit(EXIT_FAILURE);
 }
 putchar((*stack)->n);
